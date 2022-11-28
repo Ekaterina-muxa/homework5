e29b56335380f0ec7dec7040352d51cd8edc0064
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-28 20:40:02 +0200

    задача 1

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void Sqr01 (int N)
+{
+    for (int i = 1; i<= N; i++)
+    {
+        Console.WriteLine($"{i} - > {i * i * i}");
+    
+    }
+}
+Console.Write ("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Sqr01(N);
+
